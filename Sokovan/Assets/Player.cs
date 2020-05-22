bfc35a4e16316d8bab09a539db065a2a4665e23a
GodUser <jijone1005@naver.com>
2020-05-22 17:14:14 +0900

    3주차 v0.1

---
+++ Sokovan/Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 카멜명명 법
+public class Player : MonoBehaviour // class이름은 대문자로 시작 새로운 단어가 나오면 대문자로 시작
+{
+
+    public float speed = 10f;
+    private Rigidbody playerRigidbody;  // 변수이름은 첫글자는 소문자
+
+    // 게임이 처음 시작되었을 때 한번 실행
+    void Start()  // 함수는 첫글자가 대문자
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // 화면이 한번 깜빡일 때 한번 실행
+    // 영화 초당 24프레임 모바일 1초 30프레임 PC게임 1초 60프레임
+    // 콘솔 게임 1초 30프레임
+    // 1초에 대략 60번 실행 but 사양에 따라 다름
+    // 몇 번 실행 되는지는 정해져 있지 않음
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    playerRigidbody.AddForce(speed, 0, 0);  // 이 코드 보다는 아래 코드가 더 효율적이고 우아함
+        //}
+
+
+        // A <-                 -> D
+        // -1.0  -0.5  0   0.5  1.0
+        // 조이스틱을 왼쪽으로 살살 밀면 
+        float inputX = -Input.GetAxis("Horizontal"); // Horizontal 조이스틱 대응 가능, 조이스틱처럼 살짝 미는걸 구현하기 위해 숫자로 받음
+
+        float inputZ = Input.GetAxis("Vertical");
+
+        float fallSpeed = playerRigidbody.velocity.y;
+
+        //playerRigidbody.AddForce(inputX * speed, 0, inputZ * speed);  // 이거는 힘을 주는 경우(관성이 있음)
+        Vector3 velocity = new Vector3(inputX, 0, inputZ); // 이거는 속도값을 바로 줌(관성이 없음)
+
+        // (inputX * speed,0,inputZ * speed)
+        velocity = velocity * speed;
+
+        velocity.y = fallSpeed;
+
+        //(inputX * speed, fallSpeed, inputZ * speed)
+        playerRigidbody.velocity = velocity;
+
+        // 조이스틱
+    }
+}
---
+++ Sokovan/Assets/Rotator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+
+    //public Transform myTransform;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    myTransform.Rotate(60, 60, 60);
+    //    // transform.Rotate(60, 60, 60);  // short-cut : Transform 변수를 만들 필요없이 가능
+    //}
+
+    // Update는 대략 1초에 60번
+    void Update()
+    {
+        // 1번에 60도
+        transform.Rotate(60 * Time.deltaTime, 60 * Time.deltaTime, 60 * Time.deltaTime);
+
+        // Time.deltaTime 은 화면이 한 번 깜박이는 시간 = 한 프레임의 시간
+        // 화면을 60번 깜빡이면 (초당 60프레임) 1/60 초
+        // 화면을 200번 깜빡이면 (초당 200프레임) 1/200 초
+    }
+
+    // 60 * 60 = 3600 1초에 3600도 회전
+
+    // 1초에 총 1m
+
+    // Update 함수는 화면이 한번 깜빡이면 실행됨
+    // Update { 한번에 1m }
+
+    // 조선컴 : 1초에 30번
+    // 한번에 1m * 30 => 1초에 30m ==> 30 * (1/30) = 1m
+
+
+
+    // 고사양 컴퓨터: 1초에 60번
+    // 한번에 1m * 60 => 1초에 60m ==> 60 * (1/60) = 1m
+    // 1m * (1/60) * 60 = 1m
+}
